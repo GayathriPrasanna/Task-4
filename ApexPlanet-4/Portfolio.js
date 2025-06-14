ad6c759a64c9cff489ab852b7d8b073b57ec61ac
Gayathri Prasanna <gayathri162004@gmail.com>
2025-06-14 12:28:57 +0530

    updated the files

--- ApexPlanet-4/Portfolio.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ["Problem Solver","Web Developer","Java and DSA Enthusiast"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
